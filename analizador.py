c452e72044cb66d7de3069ef5daece5e6fff6cf0
Casc0 <fabern2110@gmail.com>
2026-04-22 19:34:27 -0300

    miau

---
+++ analizador.py
@@ -0,0 +1,186 @@
+from unittest import case
+
+
+FILE = "input.txt"
+
+def main() -> None:
+	with open(FILE, 'r') as file:
+		while True:
+			char = file.read(1)
+			recognize_lexeme(char, file)
+
+			
+	
+def recognize_lexeme(char: str, file) -> int:
+	token = tokens.get(char)
+	
+	match token:
+		case "ID":
+			error = caseID(char, file)
+		case "NUM":
+			error = caseNUM(char, file)
+		case "LLAVE_ABRE":
+			comentario(file)
+		case "IGNORE": #ignore whitespace
+			return 0
+
+
+def comentario(file) -> None:
+	#hacer error!!!!
+	while True:
+		char = file.read(1)
+		if char == 'LLAVE_CIERRA':
+			return 0
+			
+def caseID(file) -> None:
+	#hacer error!!!!
+	while True:
+		char = file.read(1)
+		token = tokens.get(char)
+		match token:
+			case "ID":
+				continue
+			case "NUM":
+				continue
+			case _:
+				return errors.get("ID_ERROR")
+			
+def caseNUM(file) -> None:
+	#hacer error!!!!
+	while True:
+		char = file.read(1)
+		token = tokens.get(char)
+		match token:
+			case "NUM":
+				continue
+			case _:
+				return errors.get("NUM_ERROR")
+            
+
+
+tokens ={
+	"(" : 'PAR_ABRE',
+    ")" : 'PAR_CIERRA',
+    "," : 'COMA',
+    "." : 'PUNTO',
+    ";" : 'PUNTO_COMA',
+    ":" : 'DOS_PUNTOS',
+	"{" : 'LLAVE_ABRE',
+	"}" : 'LLAVE_CIERRA',
+	"+" : 'OP_ARIT_ADD',
+	"-" : 'OP_ARIT_SUB',
+	"*" : 'OP_ARIT_MUL',
+	"/" : 'OP_ARIT_DIV',
+	"=" : 'OP_REL_EQUAL',
+	"<>" : 'OP_REL_NOT_EQUAL',
+	"<" : 'OP_REL_LTT',
+	">" : 'OP_REL_GTT',
+	"<=" : 'OP_REL_LTE',
+	">=" : 'OP_REL_GTE',
+	" " : 'IGNORE',
+	"\t" : 'IGNORE',
+	"\n" : 'IGNORE'
+}
+
+keyword = {
+	"program" : 'PROGRAM',
+	"var" : 'VAR',
+	"integer" : 'INTEGER',
+	"boolean" : 'BOOLEAN',
+	"function" : 'FUNCTION',
+	"procedure" : 'PROCEDURE',
+	"begin" : 'BEGIN',
+	"end" : 'END',
+	"if" : 'IF',
+	"then" : 'THEN',
+    "else" : 'ELSE',
+	"while" : 'WHILE',
+	"do" : 'DO',
+	"read" : 'READ',
+	"write" : 'WRITE',
+	"true" : 'TRUE',
+	"false" : 'FALSE',
+	"or" : 'OR',
+	"and" : 'AND',
+	"not" : 'NOT'
+}
+
+letter = {
+	"A" : 'A',
+	"B" : 'B',
+	"C" : 'C',
+	"D" : 'D',
+	"E" : 'E',
+	"F" : 'F',
+	"G" : 'G',
+	"H" : 'H',
+	"I" : 'I',
+	"J" : 'J',
+	"K" : 'K',
+	"L" : 'L',
+	"M" : 'M',
+	"N" : 'N',
+	"O" : 'O',
+	"P" : 'P',
+	"Q" : 'Q',
+	"R" : 'R',
+	"S" : 'S',
+	"T" : 'T',
+	"U" : 'U',
+	"V" : 'V',
+	"W" : 'W',
+	"X" : 'X',
+	"Y" : 'Y',
+	"Z" : 'Z',
+	"a" : 'a',
+	"b" : 'b',
+	"c" : 'c',
+	"d" : 'd',
+	"e" : 'e',
+	"f" : 'f',
+	"g" : 'g',
+	"h" : 'h',
+	"i" : 'i',
+	"j" : 'j',
+	"k" : 'k',
+	"l" : 'l',
+	"m" : 'm',
+	"n" : 'n',
+	"o" : 'o',
+	"p" : 'p',
+	"q" : 'q',
+	"r" : 'r',
+	"s" : 's',
+	"t" : 't',
+	"u" : 'u',
+	"v" : 'v',
+	"w" : 'w',
+	"x" : 'x',
+	"y" : 'y',
+	"z" : 'z'
+}
+
+digit = {
+	"0" : '0',
+	"1" : '1',
+	"2" : '2',
+	"3" : '3',
+	"4" : '4',
+	"5" : '5',
+	"6" : '6',
+	"7" : '7',
+	"8" : '8',
+	"9" : '9'
+}
+
+tokens.update({letter: 'ID' for letter in letter})
+tokens.update({digit: 'NUM' for digit in digit})
+
+errors = {
+	"ID_ERROR" : "Error: Lexema no reconocido. Se esperaba un ID o un NUM.",
+	"NUM_ERROR" : "Error: Lexema no reconocido. Se esperaba un NUM."
+}
+
+
+
+
